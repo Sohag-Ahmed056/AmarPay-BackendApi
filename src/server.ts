85e202f5af75c16afc608642002a9028ccda0861
Sohag Ahmed <sohagahmed056@gmail.com>
2025-10-13 21:39:27 +0600

    Error Solved

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from "./app";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import { enVars } from "./app/config/env";
-import { createDefaultAdmin } from "./app/utils/seedAdmin";
+
 dotenv.config()
 const port = process.env.PORT || 5000;
 
@@ -11,10 +11,9 @@ const startServer = async () => {
   try {
     await mongoose.connect(enVars.MONGODB_URI)
     console.log('Mongodb connected.')
-        await createDefaultAdmin();
 
     app.listen(port, () => {
-      console.log(`🚀 http://localhost:${enVars.PORT}`);
+      console.log(`http://localhost:${enVars.PORT}`);
     });
   } catch (error: any) {
     console.log('Error while connecting MongoDB', error.message)
